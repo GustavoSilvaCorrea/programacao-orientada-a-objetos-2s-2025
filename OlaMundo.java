c0b46afc544c5e58e94ea823a22e13c490b17c43
GUSTAVO DA SILVA CORRÊA <gustavosilva.correa@a.ucb.br>
2025-08-20 12:23:37 -0300

    Ola Mundo

---
+++ OlaMundo.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+class OlaMundo {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		JOptionPane.showMessageDialog(null,"Olá Mundo!");
+		System.exit(0);
+	}
+}
