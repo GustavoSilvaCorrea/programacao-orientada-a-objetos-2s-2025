9fc3553b76ec9421decd7935d5032d253ec01cc3
GustavoSilvaCorrea <gusta0581476@gmai.com>
2025-10-02 20:29:00 -0300

    Mudanças

--- ministeriofazenda/imovel.java
+++ ministeriofazenda/imovel.java
@@ -20,6 +20,22 @@ public class imovel {
         this.municipio = municipio;
     }
 
+    public double getAream2() {
+        return aream2;
+    }
+
+    public void setAream2(double aream2) {
+        this.aream2 = aream2;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
     public double calcularIPTU() {
         return (this.aream2 * this.municipio.getPrecom2());
     }
